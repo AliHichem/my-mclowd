87e6a4336e06b9df251ff43c93698ae2b9b2b257
Michał Dąbrowski <dabrowski@brillante.pl>
2013-02-05 23:46:43 +0100

    Changes

--- _chef/roles/web.rb
+++ _chef/roles/web.rb
@@ -3,9 +3,9 @@ description "Base mclowd web node setup."
  
 run_list(
   "recipe[mclowd::apt]",
+  "recipe[nginx::source]",
   "recipe[php]",
   "recipe[php-fpm]",
-  "recipe[nginx::source]",
   "recipe[beanstalkd]",
   "recipe[mysql::client]",
   "recipe[mysql::server]",

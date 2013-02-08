3afa9ff75488f48ad6b41f7c5963461550109320
Daniel Ursoviciu <dan.ursoviciu@aelius.fr>
2013-02-08 19:35:51 +0200

    #5 Config reflecting FOSMessage bundle addition

--- src/App/Entity/Message.php
+++ src/App/Entity/Message.php
@@ -1,17 +1,62 @@
 <?php
+namespace Acme\MessageBundle\Entity;
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+use Doctrine\ORM\Mapping as ORM;
+
+use FOS\MessageBundle\Entity\Message as BaseMessage;
+use FOS\MessageBundle\Model\ThreadInterface;
+use FOS\MessageBundle\Model\ParticipantInterface;
+use FOS\MessageBundle\Model\MessageMetadata as ModelMessageMetadata;
 
 /**
- * Description of Message
- *
- * @author dan
+ * @ORM\Entity
  */
-class Message {
-    //put your code here
-}
+class Message extends BaseMessage
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\generatedValue(strategy="AUTO")
+     */
+    protected $id;
 
-?>
+    /**
+     * @ORM\ManyToOne(targetEntity="Acme\MessageBundle\Entity\Thread", inversedBy="messages")
+     * @ORM\JoinColumn(name="thread_id", referencedColumnName="id")
+     */
+    protected $thread;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Acme\UserBundle\Entity\User")
+     * @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+     */
+    protected $sender;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Acme\MessageBundle\Entity\MessageMetadata", mappedBy="message", cascade={"all"})
+     */
+    protected $metadata;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->metadata  = new ArrayCollection();
+    }
+
+    public function setThread(ThreadInterface $thread) {
+            $this->thread = $thread;
+            return $this;
+    }
+
+    public function setSender(ParticipantInterface $sender) {
+            $this->sender = $sender;
+            return $this;
+    }
+
+    public function addMetadata(ModelMessageMetadata $meta) {
+        $meta->setMessage($this);
+        parent::addMetadata($meta);
+    }
+
+}

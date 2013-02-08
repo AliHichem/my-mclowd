6098ef450c17ad6dac356d3ba52a810945123180
Daniel Ursoviciu <dan.ursoviciu@aelius.fr>
2013-02-08 19:31:13 +0200

    #5 Added FOSMessageBundle to composer.json. Created entityes

---
+++ src/App/Entity/Message.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Message
+ *
+ * @author dan
+ */
+class Message {
+    //put your code here
+}
+
+?>
---
+++ src/App/Entity/ThreadMetadata.php
@@ -0,0 +1,42 @@
+<?php
+namespace Acme\MessageBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+use FOS\MessageBundle\Entity\ThreadMetadata as BaseThreadMetadata;
+use FOS\MessageBundle\Model\ThreadInterface;
+use FOS\MessageBundle\Model\ParticipantInterface;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="message_thread_metadata")
+ */
+class ThreadMetadata extends BaseThreadMetadata
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\generatedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Acme\MessageBundle\Entity\Thread", inversedBy="metadata")
+     */
+    protected $thread;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Acme\UserBundle\Entity\User")
+     */
+    protected $participant;
+
+    public function setThread(ThreadInterface $thread) {
+        $this->thread = $thread;
+    }
+
+    public function setParticipant(ParticipantInterface $participant) {
+        $this->participant = $participant;
+        return $this;
+    }
+
+}

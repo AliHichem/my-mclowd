aac4e9a49b585da283e1242f286e0e216264d518
Michał Dąbrowski <dabrowski@brillante.pl>
2013-02-12 16:38:44 +0100

    Adding data transformer

--- src/App/Form/NewJobType.php
+++ src/App/Form/NewJobType.php
@@ -4,18 +4,27 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use App\Entity\Job;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use App\Form\DataTransformer\IntegerToJobCategoryTransformer;
 
-class NewJobType extends AbstractType
+class NewJobType extends AbstractType  implements ContainerAwareInterface
 {
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $em = $this->container->get('doctrine')->getEntityManager();
+        $modelTransformer = new IntegerToJobCategoryTransformer($em);
         $builder
             ->add('name')
             ->add('description')
             ->add('type', 'choice', array('choices' =>  array_combine(Job::getTypes(), Job::getTypes())))
             ->add('currency', 'choice', array('choices' => array_combine(Job::getCurrencies(), Job::getCurrencies())))
-            ->add('category', 'job_category', ['empty_value' => 'Choose a category'])
+            ->add(
+                $builder
+                    ->create('category', 'job_category', ['empty_value' => 'Choose a category'])
+                    ->addModelTransformer($modelTransformer)
+            )
         ;        
 
     }
@@ -29,5 +38,10 @@ class NewJobType extends AbstractType
     public function getName()
     {
         return 'new_job';
+    }
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
     }
 }

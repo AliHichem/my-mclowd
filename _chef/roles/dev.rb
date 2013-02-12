1a100b7c7ece2f5f282da64b439886d2e18cb75b
Michał Dąbrowski <dabrowski@brillante.pl>
2013-02-12 14:43:04 +0100

    fixing role

--- _chef/roles/dev.rb
+++ _chef/roles/dev.rb
@@ -2,7 +2,7 @@ name "Dev"
 description "Base mclowd dev role."
  
 run_list(
-  "recipe[mclowd::dev]",
   "recipe[nodejs]",
-  "recipe[nodejs::npm]"
+  "recipe[nodejs::npm]",
+  "recipe[mclowd::dev]"
 )

6b34400e9645bda90c6cadc9d78cfa821ce2fc53
Michał Dąbrowski <dabrowski@brillante.pl>
2013-02-25 13:11:46 +0100

    Added category admin

---
+++ src/MC/AdminBundle/Admin/JobCategoryAdmin.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace MC\AdminBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin as BaseAdmin;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+use Knp\Menu\ItemInterface as MenuItemInterface;
+
+use App\Entity\Job;
+
+class JobCategoryAdmin extends BaseAdmin
+{
+     protected $baseRouteName = 'admin_job_category';
+     protected $baseRoutePattern = 'categories';
+
+    /**
+     * @param \Sonata\AdminBundle\Show\ShowMapper $showMapper
+     *
+     * @return void
+     */
+    protected function configureShowField(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('name')            
+            ->add('materializedPath')                        
+        ;
+    }
+    
+    /**
+     * @param \Sonata\AdminBundle\Form\FormMapper $formMapper
+     *
+     * @return void
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('name')             
+        ;
+    }
+
+    /**
+     * @param \Sonata\AdminBundle\Datagrid\ListMapper $listMapper
+     *
+     * @return void
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('id')
+            ->addIdentifier('name')
+            ->add('materializedPath')                        
+        ;
+    }
+
+    /**
+     * @param \Sonata\AdminBundle\Datagrid\DatagridMapper $datagridMapper
+     *
+     * @return void
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('name')
+        ;
+    }
+}

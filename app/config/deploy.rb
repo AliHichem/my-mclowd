52b64dcae4c4e9ae31a0bec037bd646de611bacf
Michał Dąbrowski <dabrowski@brillante.pl>
2013-03-05 11:36:13 +0100

    Finishing up recipe

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -3,39 +3,33 @@
 
 # Be more verbose by uncommenting the following line
 logger.level = Logger::MAX_LEVEL
-
+set :php_bin, '/usr/bin/php'
 set :stages, %w(staging production)
 set :stage_dir, 'app/config/deploy' # needed for Symfony2 only
 set :default_stage, "staging"
 require 'capistrano/ext/multistage'
 
-
 set :application, "mclowd"
-
 set :app_path,    "app"
 set :user,        "root"
-
-
-#role :web,        domain
-#role :app,        domain
-#role :db,         domain, :primary => true
-
 set :scm,         :git
 set :repository,  "git@github.com:tsslabs/mclowd-marketplace.git"
 set :branch,      "master"
 set :deploy_via,  :remote_cache
 
 ssh_options[:forward_agent] = true
+default_run_options[:pty] = true
 
 set :use_composer,   true
-#set :update_vendors, true
+set :update_vendors, false
 
 set :writable_dirs,     ["app/cache", "app/logs"]
 set :webserver_user,    "www-data"
 set :permission_method, :acl
 
 set :shared_files,    ["app/config/parameters.yml", "web/.htaccess", "web/robots.txt"]
-set :shared_children, ["app/logs"]
+set :shared_children, ["app/logs", "app/spool", "vendor"]
+
 
 set :model_manager, "doctrine"
 
@@ -43,7 +37,7 @@ set :use_sudo,    false
 
 set :keep_releases, 3
 
-before 'symfony:composer:update', 'symfony:copy_vendors'
+before 'symfony:composer:install', 'symfony:copy_vendors'
 
 namespace :symfony do
   desc "Copy vendors from previous release"
@@ -55,7 +49,26 @@ namespace :symfony do
       capifony_puts_ok
     end
   end
+
+  namespace :composer do
+    
+    desc "Runs composer to install vendors from composer.lock file"
+    task :install, :roles => :app, :except => { :no_release => true } do
+      capifony_pretty_print "--> Installing Composer dependencies"
+      run "cd #{latest_release} && composer install"
+      capifony_puts_ok
+    end
+
+    desc "Runs composer to update vendors, and composer.lock file"
+    task :update, :roles => :app, :except => { :no_release => true } do
+      capifony_pretty_print "--> Updating Composer dependencies"
+      run "cd #{latest_release} && composer update"
+      capifony_puts_ok
+    end
+  end
+
 end
 
+after "symfony:assets:install", "symfony:doctrine:schema:update"
 after "deploy:update", "deploy:cleanup"
 after "deploy", "deploy:set_permissions"

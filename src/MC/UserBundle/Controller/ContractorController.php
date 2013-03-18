213a4917790ba5bdd275701a6ad371bcf536261f
Michał Dąbrowski <dabrowski@brillante.pl>
2013-03-18 14:47:56 +0100

    setting up template

---
+++ src/MC/UserBundle/Controller/ContractorController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace MC\UserBundle\Controller;
+
+use App\Controller\Controller as BaseController,
+    Symfony\Component\HttpFoundation\Request,
+    Symfony\Component\HttpFoundation\RedirectResponse,
+    Symfony\Component\HttpFoundation\JsonResponse;
+
+class ContractorController extends BaseController
+{
+    
+    public function setTemplateAction(Request $request)
+    {
+        
+    }
+
+}
---
+++ src/MC/UserBundle/EventListener/ContractorTemplateListener.php
@@ -0,0 +1,22 @@
+<?php
+namespace MC\UserBundle\EventListener;
+
+use Acme\DemoBundle\Controller\TokenAuthenticatedController;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\DependencyInjection\ContainerAware;
+class ContractorTemplateListener extends ContainerAware
+{
+
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+
+        $context = $this->container->get('security.context');
+        if ($context->isGranted('ROLE_CONTRACTOR') && !$context->getToken()->getUser()->hasSelectedTemplate()) {
+            var_dump('no template'); die();
+        }
+
+        
+    }
+}

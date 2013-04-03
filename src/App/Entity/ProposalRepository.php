0bc39b8c6c1d3f0cd9f0c3f7ed103c318dcced7e
Pawel <pawel.strzyz@gmail.com>
2013-04-03 12:08:55 +0200

    file added

---
+++ src/App/Entity/ProposalRepository.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Entity;
+
+use Knp\RadBundle\Doctrine\EntityRepository;
+use Knp\DoctrineBehaviors\ORM as ORMBehaviors;
+
+use App\Entity\Proposal;
+
+class ProposalRepository extends EntityRepository
+{
+    public function getProposalsByTask($taskId)
+    {
+        $query = $this->getEntityManager()->createQuery('SELECT p FROM App\Entity\Proposal p WHERE p.task = :taskId');
+        $query->setParameter('taskId', $taskId);
+        
+        return $query->getArrayResult();
+    }
+}

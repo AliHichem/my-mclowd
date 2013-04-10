80f002f80b4880f785bf1770b759e8b169940273
Michał Dąbrowski <dabrowski@brillante.pl>
2013-04-10 11:25:17 +0200

    Cleanup

--- src/App/Resources/public/js/application.js
+++ src/App/Resources/public/js/application.js
@@ -33,4 +33,63 @@ mcApp.factory('ContractorTask', function($resource) {
 });
 
 
+mcApp.factory('ResponseHandler', function() {
+    return {
+        enabled: true,
+
+        disable: function() {
+            this.enabled = false;
+        },
+
+        enable: function() {
+            this.enabled = true;
+        },
+
+        handle: function (response) {
+            if (this['handle' + response.status]) {
+                this['handle' + response.status](response);
+            }
+        },
+
+        handle201: function() {
+        },
+
+        handle404: function() {
+            alert('Opps, we cannot process your request.');
+        },
+
+        handle422: function(response) {
+            if (this.enabled) {                
+                //Modal.showMessages('Fix the following errors', response.data.errors);
+            }
+        },
+
+        handle500: function() {
+            alert('Opps, an error appeared. Pls refresh page and try again.');
+        }
+    }
+})
+
+
+mcApp.config(function($httpProvider) {
+    var interceptor = function(ResponseHandler, $rootScope, $q) {
+
+        function intercept(response) {
+            // handle response
+            ResponseHandler.handle(response);
+
+            return response;
+        }
+
+        return function(promise) {
+            return promise.then(intercept, function(response) {
+                return $q.reject(response)
+            });
+        }
+
+    };
+    $httpProvider.responseInterceptors.push(interceptor);
+});
+
+
 window.mcApp = mcApp;
